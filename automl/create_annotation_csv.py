d2c29badc84f32557777f7e65540859256213e44
jkodandera <j.kodandera@drishti.com>
2022-12-01 12:54:13 -0500

    bug fixes

--- automl/create_annotation_csv.py
+++ automl/create_annotation_csv.py
@@ -1,22 +1,30 @@
-# Run as python create_annotation_csv.py csv_path bucket_name
+# Run as python create_annotation_csv.py csv_path gcs_path
 
 import csv
 import pandas as pd
-import numpy as np
 import math
 import defopt
+from PIL import Image
+import numpy as np
 
-def read_from_csv(csv_path: str, bucket_name: str):
+def load_image( infilename ) :
+    img = Image.open( infilename )
+    img.load()
+    data = np.asarray( img, dtype="int32" )
+    return data
+
+def read_from_csv(csv_path: str, gcs_path: str):
     df = pd.read_csv(csv_path)
     all_data = []
+    image_width = 1024
     for index, row in df.iterrows():
-        filename = row.get("patientId") + ".jpg"
+        filename = row.get("patientId") + ".dcm.png"
         label = row.get("Target")
         width = row.get("width")
         height = row.get("height")
         xmin = row.get("x")
         ymin = row.get("y")
-        gcs_uri = "gs://" + bucket_name + "/" + filename
+        gcs_uri = "gs://" + gcs_path + "/" + filename
         if math.isnan(xmin) and math.isnan(ymin):
             new_row = [
                 "UNASSIGNED",
@@ -28,10 +36,10 @@ def read_from_csv(csv_path: str, bucket_name: str):
             ymax = ymin + height
 
             # Normalizing co-ordinates
-            new_xmin = round((xmin / width), 1)
-            new_xmax = round((xmax / width), 1)
-            new_ymin = round((ymin / height), 1)
-            new_ymax = round((ymax / height), 1)
+            new_xmin = round((xmin / image_width), 1)
+            new_xmax = round((xmax / image_width), 1)
+            new_ymin = round((ymin / image_width), 1)
+            new_ymax = round((ymax / image_width), 1)
             new_row = [
                 "UNASSIGNED",
                 gcs_uri,
